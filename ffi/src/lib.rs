fef2f88329b7e06fb11e05b236218542764d7c36
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2025-09-16 15:54:13 +0200

    added ends_with function

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -18,8 +18,9 @@ use std::{
     net::IpAddr,
 };
 use wirefilter::{
-    AllFunction, AlwaysList, AnyFunction, CIDRFunction, ConcatFunction, GetType, LenFunction,
-    LowerFunction, NeverList, StartsWithFunction, Type, WildcardReplaceFunction, catch_panic,
+    AllFunction, AlwaysList, AnyFunction, CIDRFunction, ConcatFunction, EndsWithFunction, GetType,
+    LenFunction, LowerFunction, NeverList, StartsWithFunction, Type, WildcardReplaceFunction,
+    catch_panic,
 };
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -371,6 +372,15 @@ pub extern "C" fn wirefilter_add_function_to_scheme(
         }
         "wildcard_replace" => {
             return match builder.add_function(name, WildcardReplaceFunction::default()) {
+                Ok(_) => true,
+                Err(err) => {
+                    write_last_error!("{}", err);
+                    false
+                }
+            };
+        }
+        "ends_with" => {
+            return match builder.add_function(name, EndsWithFunction::default()) {
                 Ok(_) => true,
                 Err(err) => {
                     write_last_error!("{}", err);
